91997bbb14e0536da6395a8e4a38282023fa9ed0
Javier Bull <javierbullreyes@gmail.com>
2020-11-03 16:34:15 -0500

    movement rotation

---
+++ TestProject/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+    public float deltaRotation = 100f;
+    public float deltaMove = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        MoveForwardBack();
+        
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0f, -deltaRotation, 0f) * Time.deltaTime);    
+        } 
+        else if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(new Vector3(0f, deltaRotation, 0f) * Time.deltaTime);    
+        }
+        
+    }
+
+    void MoveForwardBack ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * deltaMove * Time.deltaTime);
+        } 
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * deltaMove * Time.deltaTime);
+        }
+    }
+
+    
+}
